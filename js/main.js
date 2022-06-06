48b3290a978affed71a126b6369c20e8a4af27e8
PinetheApple <jonathansibypaul@gmail.com>
2022-06-06 10:16:14 +0530

    final site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-document.querySelector('.ham').addEventListener('click', function(e){ 
-    alert('drop down coming soon 🙏')
+const menu= document.querySelector('.ham')
+const btn=document.querySelector('.button')
+const dropd=document.querySelector('.dropdown')
+var w = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+menu.addEventListener('click', function(){ 
+    btn.classList.remove('button')
+    if(menu.classList.contains('fa-bars'))
+    {
+        menu.classList.remove('fa-bars')
+        menu.classList.add('fa-xmark')
+        //stuff for menu to drop down
+        
+        dropd.classList.add('drop')
+    }
+    else{
+        menu.classList.remove('fa-xmark')
+        menu.classList.add('fa-bars')
+        //hide the menu
+        dropd.classList.remove('drop')
+    }
+    
 });
